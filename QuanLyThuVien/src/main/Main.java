7167c7ec72555fe01557c0b10319390adc2be2e0
Thăng <96054769+Asd531448@users.noreply.github.com>
2024-09-01 15:45:06 +0700

    Add files via upload

---
+++ QuanLyThuVien/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+
+import view.view_dangNhap;
+
+
+public class Main {
+	public static void main(String[] args) {
+		view_dangNhap frame = new view_dangNhap();
+		frame.setVisible(true);
+		
+//		view_trangChu frame = new view_trangChu();
+//		frame.setVisible(true);
+	}
+}
